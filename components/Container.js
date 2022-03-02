8a43b7c9f56843e267cfac0dc011f0daa9239b4c
Christian <christian.torlegard@outlook.com>
2022-03-02 17:35:43 +0100

    Working commit

---
+++ components/Container.js
@@ -0,0 +1,81 @@
+// This is a wrapper for any page we want to add the website and saves a lot of time by not re-writing our basic pages.
+import React from 'react';
+import { useColorMode, Button, Flex, Box } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import styled from '@emotion/styled';
+
+import DarkModeSwitch from '../components/DarkModeSwitch';
+
+const Container = ({ children }) => {
+  const { colorMode } = useColorMode();
+
+  const bgColor = {
+    light: 'white',
+    dark: '#171717',
+  };
+  const navHoverBg = {
+    light: 'gray.600',
+    dark: 'gray.300',
+  };
+  const color = {
+    light: 'black',
+    dark: 'white',
+  };
+
+  const StickyNav = styled(Flex)`
+    position: sticky;
+    z-index: 10;
+    top: 0;
+    backdrop-filter: saturate(180%) blur(20px);
+    transition: height 0.5s, line-height 0.5s;
+  `;
+  return (
+    <>
+      <StickyNav
+      flexDirection="row"
+      justifyContent="space-between"
+      alignItems="center"
+      maxWidth="800px"
+      minWidth="356px"
+      width="100%"
+      bg={bgColor[colorMode]}
+      as="nav"
+      px={[2, 6, 6]}
+      py={2}
+      mt={8}
+      mb={[0, 0, 8]}
+      mx="auto">
+        <Box>
+          <NextLink href='/' passHref>
+            <Button as='a' variant='ghost' p={[1, 2, 4]} _hover={{ backgroundColor: navHoverBg[colorMode] }}>
+              BetaWall
+            </Button>
+          </NextLink>
+          <NextLink href='/' passHref>
+            <Button as='a' variant='ghost' p={[1, 2, 4]} _hover={{ backgroundColor: navHoverBg[colorMode] }}>
+              Pictures
+            </Button>
+          </NextLink>
+          <NextLink href='/' passHref>
+            <Button as='a' variant='ghost' p={[1, 2, 4]} _hover={{ backgroundColor: navHoverBg[colorMode] }}>
+              Events
+            </Button>
+          </NextLink>
+        </Box>
+        <DarkModeSwitch />
+      </StickyNav>
+      <Flex
+        as='main'
+        justifyContent='center'
+        flexDirection='column'
+        bg={bgColor[colorMode]}
+        color={color[colorMode]}
+        px={[0, 4, 4]}
+        mt={[4, 8, 8]}
+      >
+        {children}
+      </Flex>
+    </>
+  );
+};
+export default Container;
---
+++ styles/theme.js
@@ -0,0 +1,45 @@
+import { chakra, extendTheme } from "@chakra-ui/react";
+import { theme as chakraTheme } from "@chakra-ui/react";
+import { createBreakpoints } from "@chakra-ui/theme-tools";
+
+const fonts = {
+  ...chakraTheme.fonts,
+  body: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
+  heading: `Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"`,
+};
+
+const breakPoints = createBreakpoints({
+  sm: "40em",
+  md: "52em",
+  lg: "64em",
+  lg: "62em",
+});
+
+const overrides = {
+  ...chakraTheme,
+  fonts,
+  breakPoints,
+  fontWeights: {
+    normal: 300,
+    medium: 600,
+    bold: 700,
+  },
+
+  // Use this instead of hardcoded code to easily change.
+  fontSizes: {
+    xs: "12px",
+    sm: "14px",
+    md: "16px",
+    lg: "18px",
+    xl: "20px",
+    "2xl": "24px",
+    "3xl": "28px",
+    "4xl": "36px",
+    "5xl": "48px",
+    "6xl": "64px",
+  },
+};
+
+const customTheme = extendTheme(overrides);
+
+export default customTheme;
